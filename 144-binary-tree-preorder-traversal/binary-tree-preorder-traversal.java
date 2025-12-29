3563f41f9b16f2f0674e1a62a290c03fef7a702b
akarshi Chaudhary <akarshichaudhary25@gmail.com>
2025-12-30 00:19:15 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (65.85%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -14,32 +14,29 @@
  * }
  */
 
-// class Solution {
-//     public List<Integer> preorderTraversal(TreeNode root) {
-//         ArrayList<Integer>res = new ArrayList<>();
-//         rcrsive(root, res);
-//         return res;
-//     }
-//     public  void rcrsive(TreeNode root, ArrayList<Integer>res){
-//         if(root == null) return;
-//             res.add(root.val);
-//             rcrsive(root.left,res);
-//             rcrsive(root.right,res);
-//     }
-// }
-class Solution{
-    ArrayList<Integer>res = new ArrayList<>();
-    public List<Integer> preorderTraversal(TreeNode root){
-        if(root == null)
-            return res;
-        res.add(root.val);
-        preorderTraversal(root.left);
-        preorderTraversal(root.right);
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer>res = new ArrayList<>();
+        rcrsive(root, res);
         return res;
     }
-    public void chnce(TreeNode root, ArrayList<Integer>res){
-        // for(int val: res){
-        //     System.out.print(val + " ");
-        // }
+    public  void rcrsive(TreeNode root, ArrayList<Integer>res){
+        if(root == null) return;
+            res.add(root.val);
+            rcrsive(root.left,res);
+            rcrsive(root.right,res);
     }
 }
+// class Solution{
+//     ArrayList<Integer>res = new ArrayList<>();
+//     public List<Integer> preorderTraversal(TreeNode root){
+//         if(root == null)
+//             return res;
+//         res.add(root.val);
+//         preorderTraversal(root.left);
+//         preorderTraversal(root.right);
+//         return res;
+//     }
+//     public void chnce(TreeNode root, ArrayList<Integer>res){
+//     }
+// }
