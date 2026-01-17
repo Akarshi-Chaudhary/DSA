adaa81214b7ff42cea949c9ba769f793eee5acf3
akarshi Chaudhary <akarshichaudhary25@gmail.com>
2026-01-17 23:05:08 +0530

    Time: 8 ms (23.69%) | Memory: 46.9 MB (38.23%) - LeetSync

---
+++ 2825-minimize-string-length/minimize-string-length.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minimizedStringLength(String s) {
+        Set<Character> a = new HashSet<>();
+        for(char c : s.toCharArray()){
+            a.add(c);
+     
+        }
+        return a.size();
+    }
+}
