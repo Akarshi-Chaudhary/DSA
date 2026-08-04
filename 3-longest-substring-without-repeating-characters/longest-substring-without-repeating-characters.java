f553da48e7fbfc0ce0b784bfedccec7a21008ed4
akarshi Chaudhary <akarshichaudhary25@gmail.com>
2026-08-05 00:06:00 +0530

    Time: 6 ms (72.35%) | Memory: 47.8 MB (5.23%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -22,22 +22,44 @@
 //     }
 // }
 
+// class Solution{
+//     public int lengthOfLongestSubstring(String s){
+//         HashMap<Character, Integer> map = new HashMap<>();
+        
+//         int left = 0, max = 0;
+
+//         for(int right = 0; right < s.length(); right++){
+            
+//             char ch = s.charAt(right);
+
+//             if(map.containsKey(ch)){
+//                 left = Math.max(left, map.get(ch) + 1);
+//             }
+//             map.put(ch, right);
+//             max = Math.max(max, right - left + 1);
+//         }
+//         return max;
+//     }
+// }
+
 class Solution{
     public int lengthOfLongestSubstring(String s){
-        HashMap<Character, Integer> map = new HashMap<>();
-        
-        int left = 0, max = 0;
+        int[] lastIndex = new int[128];
+        Arrays.fill(lastIndex, -1);
+
+        int left = 0;
+        int maxlen = 0;
 
         for(int right = 0; right < s.length(); right++){
-            
             char ch = s.charAt(right);
 
-            if(map.containsKey(ch)){
-                left = Math.max(left, map.get(ch) + 1);
+            if(lastIndex[ch] >= left){
+                left = lastIndex[ch] + 1;
             }
-            map.put(ch, right);
-            max = Math.max(max, right - left + 1);
+            lastIndex[ch] = right;
+
+            maxlen = Math.max(maxlen, right - left + 1);
         }
-        return max;
+        return maxlen;
     }
 }
