3471929a189019f9ba27b6b409544f7f86f1116e
akarshi Chaudhary <akarshichaudhary25@gmail.com>
2026-09-12 12:08:27 +0530

    Time: 3 ms (47.10%) | Memory: 45.1 MB (40.61%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,4 +1,37 @@
+// class Solution {
+//     public int[] intersection(int[] nums1, int[] nums2) {
+        
+//         Set<Integer> set1 = new HashSet<>();
+//         Set<Integer> set2 = new HashSet<>();
+
+//         for(int num : nums1){
+//             set1.add(num);
+//         }
+
+//         for(int num : nums2){  
+//             set2.add(num);  
+//         }
+
+//         int[] ans = new int[set1.size()];
+        
+//         int k = 0;
+
+//         for(int num : set1){
+
+//             if(set2.contains(num)){
+//                 ans[k] = num;
+
+//                 k = k + 1;
+//             }
+//         }
+        
+//         return Arrays.copyOfRange(ans, 0 , k);
+//     }
+// }
+
+
 class Solution {
+
     public int[] intersection(int[] nums1, int[] nums2) {
         
         Set<Integer> set1 = new HashSet<>();
@@ -12,19 +45,41 @@ class Solution {
             set2.add(num);  
         }
 
-        int[] ans = new int[set1.size()];
+        if(set1.size() < set2.size()){
+
+            int[] ans = new int[set1.size()];
         
-        int k = 0;
+            int k = 0;
 
-        for(int num : set1){
+            for(int num : set1){
 
-            if(set2.contains(num)){
-                ans[k] = num;
+                if(set2.contains(num)){
+                    ans[k] = num;
 
-                k = k + 1;
+                    k = k + 1;
+                }
             }
+            return Arrays.copyOfRange(ans, 0 , k);
+
         }
         
-        return Arrays.copyOfRange(ans, 0 , k);
+        else
+        {
+
+            int[] ans = new int[set2.size()];
+        
+            int k = 0;
+
+            for(int num : set2){
+
+                if(set1.contains(num)){
+                    ans[k] = num;
+
+                    k = k + 1;
+                }
+            }
+            return Arrays.copyOfRange(ans, 0 , k);
+        }
+
     }
 }
