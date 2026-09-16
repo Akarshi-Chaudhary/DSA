c15b59f0674473538284f1a79a264620ccf4beae
akarshi Chaudhary <akarshichaudhary25@gmail.com>
2026-09-17 00:06:24 +0530

    Time: 3 ms (17.81%) | Memory: 47.9 MB (20.28%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -13,16 +13,51 @@
 //     }
 // }
 
+// class Solution{
+//     public void moveZeroes(int[] nums){
+//         int index = 0;
+//         for(int i = 0; i < nums.length; i++){
+//             if(nums[i] != 0){
+//                 nums[index++] =  nums[i];
+//             }
+//         }
+//         while(index <nums.length){
+//             nums[index++] = 0;
+//         }
+//     }
+// }
+
+// class Solution{
+//     public void moveZeroes(int[] nums){
+
+//         int j = 0;
+
+//         for(int i = 0; i < nums.length; i++){
+          
+//             if(nums[i] != 0){
+//                 nums[j] = nums[i];
+//                 j = j + 1;
+//             }
+           
+//         }
+//         while(j < nums.length){
+//             nums[j] = 0;
+//             j = j + 1;
+//         }
+//     }
+// }
+
 class Solution{
     public void moveZeroes(int[] nums){
-        int index = 0;
+        int j = 0;
+
         for(int i = 0; i < nums.length; i++){
-            if(nums[i] != 0){
-                nums[index++] =  nums[i];
+            if(nums[i] !=0 ){
+                int temp = nums[i];
+                nums[i] = 0;
+                nums[j] = temp;
+                j = j + 1;
             }
-        }
-        while(index <nums.length){
-            nums[index++] = 0;
         }
     }
 }
